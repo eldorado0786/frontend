9657ca02459e2b95cd0052ce348d09374a48a921
eldorado0786 <mdatifraza926@gmail.com>
2025-03-15 01:11:15 +0530

    Testing Component Section

---
+++ src/testing/Testing.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayHeader from "@/components/ui-components/PlayHeader";
+
+const Testing = () => {
+  return (
+    <>
+      {/* <PlayHeader/> */}
+      testing
+    </>
+  );
+};
+
+export default Testing;
